33095914ae0a278eedcde18bc76633a417afddca
Semen <fiasko123@ukr.net>
2023-11-04 15:08:27 +0200

    [PWP-19] about us page

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function AboutUs() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>
+                Welcome to our company's "About Us" page. We are a team of passionate individuals dedicated to providing excellent products and services to our customers.
+            </p>
+            <p>
+                Our mission is to deliver high-quality solutions that meet the needs of our clients and make a positive impact in the industry.
+            </p>
+            <p>
+                Feel free to reach out to us if you have any questions or would like to learn more about our company. We're here to help!
+            </p>
+        </div>
+    );
+}
+
+export default AboutUs;
+
