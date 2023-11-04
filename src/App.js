955d0bfb2274aac0eb34fdda974bdbed48996bbd
Semen <fiasko123@ukr.net>
2023-11-04 15:05:12 +0200

    [PWP-18] added router in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Login from './pages/Login';
+import AboutUs from './pages/AboutUs';
+
 import './App.css';
+
 
 function App() {
   return (
-    <>ss</>
+    <>
+      <Router>
+        <Main></Main>
+
+        <Routes>
+          <Route exact path="/Login" element={<Login />}></Route>
+          <Route exact path="/AboutUs" element={<AboutUs />}></Route>
+        </Routes>
+
+      </Router>
+    </>
   );
 }
 
